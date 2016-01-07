8f25484955711058072079e6e8613694a162ac15
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2016-01-07 07:59:24 -0200

    Demonstrate jQuery UI dialog usage

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
-require('jquery-ui');
 var $ = require('jquery');
+require('jquery-ui/ui/widgets/dialog');
 var ko = require('knockout');
 console.log($, ko);
 document.body.innerText = require('./dependency.js');
+jQuery(function(){
+  jQuery('<div title="Test">message</div>').dialog();
+});
