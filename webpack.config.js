9293e825184afc3ddffed0252ca5bbef65e53892
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2016-01-07 08:03:01 -0200

    some refactoring of the configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var PROD = JSON.parse(process.env.PROD || '0');
 var fs = require('fs');
 var AssetsPlugin = require('assets-webpack-plugin');
 var plugins = [ new AssetsPlugin() ];
-if (PROD) plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+if (PROD) plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true }));
+//  , exclude: /(\.min|knockout-latest)\.js/
 plugins.push(function(){
   this.plugin('done', function(stats){
     var data = fs.readFileSync('index.html.tmpl', 'utf8');
@@ -30,39 +31,39 @@ var smSuffix = SMAPS ? '?sourceMap' : ''; //'?-minimize&-autoprefixer';
 var USE_MINIFIED_VENDORS = true;
 
 module.exports = {
-    entry:
-    { main: './src/js/main'
-      ,'theme-a': './src/css/theme-a.js'
-      ,'theme-b': './src/css/theme-b.js'
+  entry:
+  { main: './src/js/main'
+    , 'theme-a': './src/css/theme-a.js'
+    , 'theme-b': './src/css/theme-b.js'
+  }
+  ,output: {
+    publicPath: 'assets/'
+    , path: './dist'
+    , filename: PROD ? '[name]-[hash].min.js' : '[name].js'
+  }
+  ,resolveLoader: {
+    alias: { 'ko-loader': __dirname + '/loaders/ko-loader'
     }
-    ,output: {
-        publicPath: 'assets/',
-        path: './dist',
-        filename: PROD ? '[name]-[hash].min.js' : '[name].js'
-    }
-    ,resolveLoader: {
-      alias: { 'ko-loader': __dirname + '/loaders/ko-loader'
-      }
-    }
-    ,module: {
-      noParse: [ 'jquery'
-        , 'jquery-ui'
-      ]
-      ,loaders: [
-        { test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('style', 'css' + smSuffix + '!sass' + smSuffix) }
-          //loader: ['style', 'css' + smSuffix, 'sass' + smSuffix] }
-        ,
-        { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css' + smSuffix) }
-          //loader: 'style!css'
-        , { test: /\.coffee$/, loader: 'coffee' }
-        , { test: /\.png$/, loader: 'url?mimetype=image/png&limit=50000'}
-        , { test: /knockout-latest\.debug\.js$/, loader: 'ko-loader'}
-      ]
-    }
-    , devtool: SMAPS ? 'source-map' : ''
-    , plugins: plugins
-    , cache: true // speed up watch mode (in-memory caching only)
+  }
+  ,module: {
+    loaders: [
+      { test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css' + smSuffix + '!sass' + smSuffix) }
+        //loader: ['style', 'css' + smSuffix, 'sass' + smSuffix] }
+      ,
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css' + smSuffix) }
+        //loader: 'style!css'
+      , { test: /\.coffee$/, loader: 'coffee' }
+      , { test: /\.png$/, loader: 'url?mimetype=image/png&limit=50000'}
+      , { test: /knockout-latest\.debug\.js$/, loader: 'ko-loader'}
+    ]
+  }
+  , devtool: SMAPS ? 'source-map' : ''
+  , plugins: plugins
+  , cache: true // speed up watch mode (in-memory caching only)
+  , noParse: [ 'jquery'
+    , 'jquery-ui'
+  ]
 };
 
 if (USE_MINIFIED_VENDORS) module.exports.resolve = {
